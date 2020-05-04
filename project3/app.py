651143aaacea30adccfb7f53a763543be9466cb1
koberlag <koberlag@gmail.com>
2020-05-03 22:34:26 -0500

    Setup basic flask interface

---
+++ project3/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/add",  methods=['POST'])
+def add():
+    ingredients = request.form['ingredients'].split(",")
+    return redirect(url_for("home"))
+    
+if __name__ == "__app__":
+    app.run(debug=True)
